4928c9b80b00c93bb67b27d1073f5dda1bfce215
Lupin The Destructor <lupin@Lupins-MBP.lan>
2024-03-31 23:08:06 -0400

    Adds interface CardProps for items in Card component

--- app/ui/dashboard/card/card.tsx
+++ app/ui/dashboard/card/card.tsx
@@ -1,7 +1,16 @@
 import { MdSupervisedUserCircle } from "react-icons/md";
 import styles from "./card.module.css";
 
-const Card = ({ item }) => {
+interface CardProps {
+  item: {
+    title: string;
+    number: number;
+    change: number;
+  }
+
+}
+
+const Card = ({ item }: CardProps) => {
   return (
     <div className={styles.container}>
       <MdSupervisedUserCircle size={24} />
